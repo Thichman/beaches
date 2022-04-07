c6e818d4d614fe8e3f7f806fd87c19cbcb6ebf8b
Tyson Hichman <tysonhichman@Tysons-MacBook-Pro.local>
2022-04-07 02:02:03 -0400

    3

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
   return (
     <div className="flex flex-col w-screen bg-gradient-to-r from-violet-500 to-fuchsia-500">
       <h1 className="py-6 text-4xl font-bold mx-auto">Beaches</h1>
-      <p className="mx-auto font-semibold text-gray-700">
+      <p className="mx-auto font-semibold text-black text-center w-15">
         Browse beaches in and around the Miami area to see nearby water quality and reviews from other beach
         goers. You will be able to see the most recent posts and experiences from users at 19 of the most popular
         locations in the area. Feel free to share your experience and leave a rating on the beach!
