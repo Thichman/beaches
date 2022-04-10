80c3253fe65b365c5cdf410ac8515fe9c443ac77
Tyson Hichman <tysonhichman@Tysons-MacBook-Pro.local>
2022-04-10 03:00:18 -0400

    3

--- pages/contact_us.js
+++ pages/contact_us.js
@@ -12,8 +12,8 @@ export default function contact_us() {
                 <Image
                     src="/Men_s_Soccer_Headshots_Tyson_Hichman.jpeg"
                     alt="beach pic"
-                    width={500}
-                    height={1000}
+                    width={700}
+                    height={900}
                 />
             </div>
             <div className="mx-auto text-center py-9 text-lg"> Please contact us with any further questions.</div>
